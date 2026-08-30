5aa6509acbf2d9a477e61ac3df1c61f8682f20cb
Jayaprakash A <jayaprakash99jai@gmail.com>
2026-08-30 05:47:14 +0530

    Add files via upload

---
+++ week1_Day1_brkout.spec.js
@@ -0,0 +1,44 @@
+
+let firstName = "Jai";
+console.log("firstName:", firstName);
+console.log("Type of firstName:", typeof firstName);
+
+let companyName = "TestLeaf";
+console.log("companyName:", companyName);
+console.log("Type of companyName:", typeof companyName);
+
+let mobileNumber = 2345656765;
+console.log("mobileNumber:", mobileNumber);
+console.log("Type of mobileNumber:", typeof mobileNumber);
+
+let isAutomation = true;
+console.log("isAutomation:", isAutomation);
+console.log("Type of isAutomation:", typeof isAutomation);
+
+let hasPlaywright;
+console.log("hasPlaywright:", hasPlaywright);
+console.log("Type of hasPlaywright:", typeof hasPlaywright);
+
+var age = 27;
+console.log("age:", age);
+console.log("Type of age:", typeof age);
+
+var rateOfInterest = 8/3;
+console.log("rateOfInterest:", rateOfInterest);
+console.log("Type of rateOfInterest:", typeof rateOfInterest);
+
+var maxNumber = Number.MAX_SAFE_INTEGER;
+console.log("maxNumber:", maxNumber);
+console.log("Type of maxNumber:", typeof maxNumber);
+
+var bigNumber = 9007191n;
+console.log("bigNumber:", bigNumber);
+console.log("Type of bigNumber:", typeof bigNumber);
+
+//null
+var nullValue = null;
+console.log("Type of nullValue:", typeof nullValue);
+
+//null, undefined
+var undValue;
+console.log(undValue);
